4c23d3080fb0e52d0291d4a8e931b336a27ed5d2
Ruben Rodriguez <mirodrig@hotmail.com>
2022-02-28 13:05:12 -0600

    Sección 8 - Completada

---
+++ src/app/pais/components/pais-tabla/pais-tabla.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-pais-tabla',
+  templateUrl: './pais-tabla.component.html',
+  styles: [
+  ]
+})
+export class PaisTablaComponent  {
+
+ @Input() paises: Country[] = [];
+
+  constructor( private paisService: PaisService) { }
+
+ 
+
+}
---
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-capital',
+  templateUrl: './por-capital.component.html',
+  styles: [
+  ]
+})
+export class PorCapitalComponent  {
+
+  termino = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
+
+
+  constructor(private paisService: PaisService) { }
+
+  buscar(termino: string) {
+    
+    this.hayError = false;
+    this.termino = termino;
+
+    this.paisService.buscarCapital( this.termino )
+    .subscribe( (paises) => {
+
+      console.log(paises);
+      this.paises = paises;
+      
+
+    }, (err) => {
+      this.hayError = true;
+      this.paises = [];
+    })
+  }
+    
+  
+
+}
+  
+
